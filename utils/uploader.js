da41c4898f68fe8fc8704457393c7d57fa1fbfaf
TheWicher1992 <sameer.nadeem24@gmail.com>
2021-03-16 22:25:30 +0500

    File Uploading mechanism

---
+++ utils/uploader.js
@@ -0,0 +1,35 @@
+const multer = require('multer')
+
+const uploader = (req, res, filename, cb) => {
+    var Storage = multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, './Images')
+        },
+        filename: function (req, file, callback) {
+            callback(null, filename + '_' + file.originalname);
+        }
+    })
+
+    var upload = multer({
+        storage: Storage
+    }).single('kitchen-logo')
+
+    upload(req, res, cb)
+}
+
+module.exports = uploader
+
+// app.get("/", function (req, res) {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+
+// app.post("/api/Upload", function (req, res) {
+//     upload(req, res, function (err) {
+//         if (err) {
+//             throw err
+//             return res.end("Something went wrong!");
+//         }
+//         return res.end("File uploaded sucessfully!.");
+//     });
+// });
