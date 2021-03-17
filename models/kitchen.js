547e7553cea891731a8ff5627e3cb5000ef28029
TheWicher1992 <sameer.nadeem24@gmail.com>
2021-03-17 22:35:54 +0500

    models complete

--- models/kitchen.js
+++ models/kitchen.js
@@ -1,33 +1,39 @@
-let mongoose = require('mongoose')
+const mongoose = require('mongoose')
 // kitchen schema
-let kitchenSchema  = mongoose.Schema({
+const kitchenSchema = mongoose.Schema({
 
-title:{
-    type: String,
-    required: true
-},
-logo:{
-    type : String,
-    required: true
-},
-activeHours:{
-    type : Number,
-    required: true
-},
-description:{
-    type : String,
-    required: true
-},
-chef:{
-    type : String,
-    required: true
-}//,
-//activeOrders:{
-//    type : Number,
- //   required: true
-//},
-//completedOrders:{
-  //  type : Number,
-    //required: true
-//}
+    title: {
+        type: String,
+        required: true
+    },
+    logo: {
+        type: String,
+        required: true
+    },
+    activeHours: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    chef: {
+        type: String,
+        required: true
+    },
+    activeOrders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'activeOrder'
+        }
+    ],
+    completeOrders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'completeOrder'
+        }
+    ]
 });
+
+module.exports = mongoose.model('kitchen', kitchenSchema)
--- models/user.js
+++ models/user.js
@@ -1,37 +1,52 @@
-let mongoose = require('mongoose')
+const mongoose = require('mongoose')
 // user schema
-let userSchema  = mongoose.Schema({
-
-email:{
-    type: String,
-    required: true
-},
-password:{
-    type : String,
-    required: true
-},
-address:{
-    type : String,
-    required: true
-},
-phone:{
-    type : String,
-    required: true
-},
-firstName:{
-    type : String,
-    required: true
-},
-lastName:{
-    type : String,
-    required: true
-}//,
-//activeOrders:{
-//    type : Number,
-//    required: true
-//},
-//completedOrders:{
-  //  type : Number,
-  //  required: true
-//}
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        addr: {
+            type: String,
+            require: true
+        },
+        coords: {
+            long: {
+                type: Number
+            },
+            lat: {
+                type: Number
+            }
+        }
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    activeOrders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'activeOrder'
+        }
+    ],
+    completeOrders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'completeOrder'
+        }
+    ]
 });
+
+module.exports = mongoose.model('user', userSchema)
