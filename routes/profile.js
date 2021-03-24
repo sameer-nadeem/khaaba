b4b29986798c226d6ba4932588f6e2809aa07969
faraz-karim <faraz.karim1999@gmail.com>
2021-03-24 15:30:44 +0500

    faraz karim first commit for update profile (only kitchen logo left)

--- routes/profile.js
+++ routes/profile.js
@@ -33,18 +33,152 @@ const storage = multer.diskStorage({
     }
 });
 
+const {
+    USER_ALREADY_EXISTS,
+    SERVER_ERROR,
+    INVALID_CREDITS,
+} = require('../utils/errors')
+const kitchen = require('../models/kitchen')
+// return information to the stuff
 
-// // change Password
-//         //customer
-// router.post('/change_pass/customer', async (req, res) => {
-// })
-//         //chef
-// router.post('/change_pass/chef', async (req, res) => {
-// })
+ 
+
+////// change Password
+//         customer and chef
+router.post('/change_pass/customer',  async (req, res) => { // for testing without token
+    const {
+            password,
+    } = req.body
+
+
+    try {
+
+        const salt = await bcrypt.genSalt(10)
+
+        const newPassword = await bcrypt.hash(password, salt)
+
+        Chef.updateOne({ _id:req.user.id},//"60573d5785214d0bc5b0f9b7"},//req.user.id},//  
+        {$set: 
+            {  
+                password: newPassword,
+
+            } }, 
+                function (err, docs) { 
+        if (err){ 
+            res.status(400).json({
+                error: [SERVER_ERROR]
+            })
+        } 
+        else{ 
+            return docs;
+            
+    }
+    });
+
+    } catch(err){
+        return res
+        .status(400)
+        .json({ errors: [INVALID_CREDITS] });
+    }
+})
 
 
 
-// change customer profile
+//////////          Change Kitchen Logo
+
+//////////          Change Chef profile (excluding kitchen logo)
+//router.post('/change_profile/chef', auth,  async (req, res) => { // final
+router.post('/change_profile/chef',  async (req, res) => { // for testing without token
+    const {
+        email,
+        address,
+        city,
+        phone,
+        firstName,
+        lastName,
+        title,
+        startingHour,
+        endingHour,
+        description,
+    } = req.body
+
+
+    try {
+
+        const retProfile = await Chef.findOne({_id:"60573d5785214d0bc5b0f9b7"})//req.user.id}) // get the chef object from db
+        
+        if (email != (retProfile).email ) // check if new email is different from stored
+        {
+            const isChefReg = await User.exists({   // check if the new email is already in use (also appliesto using the same one again)
+                email
+            })
+
+            if (isChefReg ) {
+                return res.status(400).json({
+                    errors: [USER_ALREADY_EXISTS]
+                })
+            }
+        }
+       
+        const mapuri = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${config.get('google_maps_api_key')}`
+
+        const result = await axios.get(mapuri)
+        const coords = result.data.results[0].geometry.location
+
+        Chef.updateOne({ _id:"60573d5785214d0bc5b0f9b7"},//req.user.id},//  
+            {$set: 
+                {   email:email,
+                    firstName:firstName,
+                    lastName:lastName, 
+                    phone:phone,
+                    address: {addr: toString(address).toLowerCase(),city:toString(city).toLowerCase(), coords:coords},
+                    city: city,
+
+                } }, 
+                    function (err, docs) { 
+            if (err){ 
+                res.status(400).json({
+                    error: [SERVER_ERROR]
+                })
+            } 
+            else{ 
+                return docs;
+                
+        }
+        });
+
+        console.log("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
+        console.log(retProfile.kitchen)
+        kitchen.updateOne({ _id:(retProfile.kitchen)},//req.user.id},  
+        {$set: 
+            {   
+                title: title,
+                activeHours: {startingHour: startingHour, endingHour: endingHour},
+                description: description,
+            }}, 
+                function (err, docs) { 
+        if (err){ 
+            res.status(400).json({
+                error: [SERVER_ERROR]
+            })
+        } 
+        else{ 
+            return docs;
+            
+    }
+    });
+
+    } catch(err){
+        console.error(err.message)
+        res.status(400).json({
+        error: [SERVER_ERROR]
+        })
+    }
+})
+
+
+
+/////////           Change Customer Profile
 router.post('/change_profile/customer',auth,  async (req, res) => {
 
     const {
@@ -57,69 +191,54 @@ router.post('/change_profile/customer',auth,  async (req, res) => {
     } = req.body
 
     try {
+
+        // retrive obect
+        const retProfile = User.findOne({_id: req.user.id})
         
+        if (email != (await retProfile).email )
+        {
             const isUserReg = await User.exists({   // check if the new email is already in use (also appliesto using the same one again)
                 email
             })
 
-            if (isUserReg) {
+            if (isUserReg ) {
                 return res.status(400).json({
-                    errors: ['user_already_exists']
+                    errors: [USER_ALREADY_EXISTS]
                 })
             }
-
+        }
             const mapuri = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${config.get('google_maps_api_key')}`
 
             const result = await axios.get(mapuri)
             const coords = result.data.results[0].geometry.location
 
-            // email,
-            // password,
-            // address: {
-            //     addr: address,
-            //     city,
-            //     coords
-            // },
-            // phone,
-            // firstName,
-            // lastName
-
-
-            User.updateOne({id: req.user.id},  
+            User.updateOne({_id: req.user.id},  
                 {$set: 
                     {   email:email,
                         firstName:firstName,
                         lastName:lastName, 
+                        city: city,
                         phone:phone,
-                        address: {addr: address,city: city, coords:coords}  } }, 
-                        //address: {addr: address, coords:coords}  } },
+                        address: {addr: toString(address).toLowerCase(),city: toString(city).toLowerCase(), coords:coords}  
+                    } }, 
                         function (err, docs) { 
                 if (err){ 
                     res.status(400).json({
-                        error: ['server_error']
+                        error: [SERVER_ERROR]
                     })
                 } 
                 else{ 
-                    console.log(docs);
-                    
-
-                } 
+                    return docs;
+            }
             });
-
-
 
     } catch (err) {
         console.error(err.message)
         res.status(400).json({
-        error: ['server_error']
+        error: [SERVER_ERROR]
         })
     }
 })
-
-
-// // change chef profile
-// router.post('/change_profile/chef', async (req, res) => {
-// })
 
 
 module.exports = router
