a83114c0663d087d9d6e7a08e93398dbec1df2f7
TheWicher1992 <sameer.nadeem24@gmail.com>
2021-03-28 18:22:59 +0500

    issues fixed in ordermanagement controller

--- routes/orderManagement.js
+++ routes/orderManagement.js
@@ -1,141 +1,129 @@
 const express = require('express')
 const router = express.Router()
-
 const auth = require('../middlewares/auth')
-
 const Kitchen = require('../models/kitchen')
-const ActiveOrders = require('../models/activeOrders')
+const ActiveOrders = require('../models/activeOrder')
 
 const {
     SERVER_ERROR,
     UPDATE_ERROR,
 } = require('../utils/errors')
-const user = require('../models/user')
-
-
-
-
-
-
-
+const User = require('../models/user')
 
 // chef accept order
-router.post('/orderManagement/accept',auth,  async (req, res) => { 
+router.post('/orderManagement/accept', auth, async (req, res) => {
 
     try {
         const {
             orderID,
-    } = req.body
+        } = req.body
 
-const acceptedOrder =  await ActiveOrders.findOne({_id:orderID})
-acceptedOrder.status = 'Preparing'
+        const acceptedOrder = await ActiveOrders.findOne({ _id: orderID })
+        acceptedOrder.status = 'Preparing'
 
-acceptedOrder.save(function(err) { 
-         
-        if (err){
-            console.error(err)
-            return res.status(400).json({errors: [UPDATE_ERROR]})
-        }
-        else
-            {
+        acceptedOrder.save(function (err) {
+
+            if (err) {
+                console.error(err)
+                return res.status(400).json({ errors: [UPDATE_ERROR] })
+            }
+            else {
                 return res.status(200).end()
             }
-    
+
         });
-        
-}catch(err)
-{
-    console.error(err)
-    res.status(400).json({
-        errors: [SERVER_ERROR]
-    })
-}
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({
+            errors: [SERVER_ERROR]
+        })
+    }
 });
 
 
 //chef update order status ready
-router.post('/orderManagement/ready',auth,  async (req, res) => { 
+router.post('/orderManagement/ready', auth, async (req, res) => {
 
     try {
         const {
             orderID,
-    } = req.body
+        } = req.body
 
-    const acceptedOrder =  await ActiveOrders.findOne({_id:orderID})
-    acceptedOrder.status = 'Ready'
-    
-    acceptedOrder.save(function(err) { 
-             
-            if (err){
+        const acceptedOrder = await ActiveOrders.findOne({ _id: orderID })
+        acceptedOrder.status = 'Ready'
+
+        acceptedOrder.save(function (err) {
+
+            if (err) {
                 console.error(err)
-                return res.status(400).json({errors: [UPDATE_ERROR]})
+                return res.status(400).json({ errors: [UPDATE_ERROR] })
             }
-            else
-                {
-                    return res.status(200).end()
-                }
-        
-            });
-        
-}catch(err)
-{
-    console.error(err)
-    res.status(400).json({
-        errors: [SERVER_ERROR]
-    })
-}
+            else {
+                return res.status(200).end()
+            }
+
+        });
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({
+            errors: [SERVER_ERROR]
+        })
+    }
 });
 
 
 //customer cancel order
-router.post('/orderManagement/cancel',auth,  async (req, res) => {
+router.post('/orderManagement/cancel', auth, async (req, res) => {
 
     try {
 
 
         const {
             orderID,
-    } = req.body
+        } = req.body
 
 
 
-const kitchen =  await Kitchen.findOne({_id:req.user.kitchen})
+        const kitchen = await Kitchen.findOne({ _id: req.user.kitchen })
 
-const cancelledOrder = kitchen.activeOrders.filter(_id == orderID)
-cancelledOrder.status = 'Cancelled';
-await cancelledOrder.save(function(err) { 
-             
-    if (err){
+        const cancelledOrder = kitchen.activeOrders.filter(_id == orderID)
+        cancelledOrder.status = 'Cancelled';
+        await cancelledOrder.save(function (err) {
+
+            if (err) {
+                console.error(err)
+                return res.status(400).json({ errors: [UPDATE_ERROR] })
+            }
+            else {
+                return res.status(200).end()
+            }
+
+        });
+
+        // add to completed
+        kitchen.completedOrders.push(cancelledOrder)
+
+        const newCompleted = new completedOrders
+
+        newCompleted = cancelledOrder;
+        ///////
+
+        kitchen.update(
+            {},
+            { $pull: { activeOrders: { _id: orderID } } },
+            { multi: true }
+        )
+
+
+    } catch (err) {
         console.error(err)
-        return res.status(400).json({errors: [UPDATE_ERROR]})
+        res.status(400).json({
+            errors: [SERVER_ERROR]
+        })
     }
-    else
-        {
-            return res.status(200).end()
-        }
-
-    });
-
-// add to completed
-kitchen.completedOrders.push(cancelledOrder)
-
-const newCompleted = new completedOrders
-
-newCompleted = cancelledOrder;
-///////
-
-kitchen.update(
-    { },
-    { $pull: { activeOrders : {_id : orderID}  } },
-    { multi: true }
-)
-
-        
-}catch(err)
-{
-    console.error(err)
-    res.status(400).json({
-        errors: [SERVER_ERROR]
-    })
-}
 });
+
+
+module.exports = router
--- routes/setRoutes.js
+++ routes/setRoutes.js
@@ -2,18 +2,16 @@ const authRoutes = require('./auth')
 const kitchenRoutes = require('./chef')
 const userRoute = require('./user')
 const chefRoute = require('./chef')
-const recommendationRoutes = require('./recommendations')
-
+const recomRoutes = require('./recommendations')
+const orderRoutes = require('./orderManagement')
 const profileRoutes = require('./profile')
 
 module.exports = (app) => {
     app.use('/auth', authRoutes)
     app.use('/kitchen', kitchenRoutes)
-
     app.use('/user', userRoute)
     app.use('/chef', chefRoute)
-
     app.use('/profile', profileRoutes)
-    app.use('/recommendations', recommndationRoutes)
-    app.use('/orderManagement', recommndationRoutes)
+    app.use('/recommendations', recomRoutes)
+    app.use('/orderManagement', orderRoutes)
 }
